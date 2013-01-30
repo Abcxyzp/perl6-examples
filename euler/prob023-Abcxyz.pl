3462349ac916e86807d2d934948e849dce77fcc6
Abcxyz <Abcxyz@cpan.org>
2013-01-30 03:32:38 +0000

    Update euler/prob023-Abcxyz.pl

--- euler/prob023-Abcxyz.pl
+++ euler/prob023-Abcxyz.pl
@@ -1,7 +1,7 @@
 use v6;
 
-sub sum-of-proper-divisors ( $n is copy ) {	# This could be opted faster by sieving to get a list of results directly, but not necessary here, since the slowest section is on the later loop instead.
-	my $sum = 1 - $n;
+sub sum-of-divisors ( $n is copy ) {	# This could be opted faster by sieving to get a list of results directly, but not necessary here, since the slowest section is on the later loop instead.
+	my $sum = 1;
 	my $tmp;
 	loop ( my $p = 2; $p**2 <= $n; $p += $p==2 ?? 1 !! 2 ) {
 		next unless $n %% $p;
@@ -16,14 +16,14 @@ sub sum-of-proper-divisors ( $n is copy ) {	# This could be opted faster by siev
 	return $sum;
 }
 
-my @abundant-ns = grep { $_ < sum-of-proper-divisors $_ }, 12..28123 - 12;
+my @abundant-ns = grep { $_ + $_ < sum-of-divisors $_ }, 12..28123 - 12;
 my @result = 0, 1 xx 28123;
 
 for @abundant-ns.kv -> $i, $n {
 	# last if $n > 28123 div 2;	# This line is not necessary, since the inner loop directly quits anyway.
 	my $limit = 28123 - $i;
 	# for $i..^@abundant-ns -> $j {			# 13/15 min, LAZY list == SLOW list... Optimizing it into loop is a better choice (perl5's interpreter does this by default)
-	loop ( $j = $i; $j < @abundant-ns; $j++ ) {	# 36/41 sec, completely equivalent to the line above, but much faster.
+	loop ( my $j = $i; $j < @abundant-ns; $j++ ) {	# 36/41 sec, completely equivalent to the line above, but much faster.
 		last if @abundant-ns[$j] > $limit;
 		@result[ @abundant-ns[$j] + $n ] = 0;
 	}
