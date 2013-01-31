f3703d7d09ed543a6ae9547747af203382943e7c
Abcxyz <Abcxyz@cpan.org>
2013-01-31 06:01:29 +0000

    Update euler/prob023-Abcxyz.pl

--- euler/prob023-Abcxyz.pl
+++ euler/prob023-Abcxyz.pl
@@ -22,8 +22,8 @@ my @result = 0, 1 xx 28123;
 for @abundant-ns.kv -> $i, $n {
 	# last if $n > 28123 div 2;	# This line is not necessary, since the inner loop directly quits anyway.
 	my $limit = 28123 - $i;
-	# for $i..^@abundant-ns -> $j {			# 13/15 min, (n:. r: takes 41 min with > 4GB mem)
-	loop ( my $j = $i; $j < @abundant-ns; $j++ ) {	# 36/41 sec, (n:. r: takes 49 min with 250MB mem)
+	# for $i..^@abundant-ns -> $j {			# n: 13/15 min, (r: takes 41 min with > 4GB mem)
+	loop ( my $j = $i; $j < @abundant-ns; $j++ ) {	# n: 36/41 sec, (r: takes 49 min with 250MB mem)
 		last if @abundant-ns[$j] > $limit;
 		@result[ @abundant-ns[$j] + $n ] = 0;
 	}
